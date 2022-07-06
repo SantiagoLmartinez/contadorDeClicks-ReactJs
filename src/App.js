b6d2c165ba4fd90b77d07a5f8c817125d03b2ac4
santiago L Martinez <slm17slm17@gmail.com>
2022-07-06 00:15:13 -0300

    se crea brach dados

--- src/App.js
+++ src/App.js
@@ -9,7 +9,16 @@ function App() {
 
   }
 
+  const randomNumber = () =>{
+    return Math.floor(Math.random() * 6) + 1;
+
+  }  
+  const handleClickDado = () => {
+    setClick(parseInt(randomNumber()))
+  }
+
   const handleClickRestart = () => {
+    // setClick(parseInt(randomNumber(1, 6)))
     setClick(0)
   }
 
@@ -17,13 +26,14 @@ function App() {
     <div className="App">
 
     <div className='app-container'>
-    <h1 className='title'>Contador de clicks</h1>
+    <h1 className='title'>App Contador & Dados </h1>
       <div className='container'>
         <div className='contadorClicksContainer'>
           <h2>{click}</h2>
         </div>
         <div className='btn-container'>
-          <button className='btn' onClick={handleClick}>Click</button>
+          <button className='btn' onClick={handleClick}>Contador + 1</button>
+          <button className='btn' onClick={handleClickDado}>Tirar Dado del 1 al 6</button>
           <button className='btn' onClick={handleClickRestart}>Reiniciar</button>
         </div>
       </div>
